41ac611b3141fbebac217c7d3308e9d1c42c95c5
Karina Antonio <karinafantonio@gmail.com>
2016-07-04 03:22:45 -0700

    Strip whitespace from each club field, just in case

--- da_club_spreadsheet_scraper.py
+++ da_club_spreadsheet_scraper.py
@@ -12,11 +12,11 @@ def main():
             club_is_active = bool(row.cells[DATES_INDEX].text.strip())
             if club_is_active:
                 clubs.append({
-                    "name": row.cells[NAME_INDEX].text,
-                    "days": row.cells[DAYS_INDEX].text,
-                    "dates": row.cells[DATES_INDEX].text,
-                    "time": row.cells[TIME_INDEX].text,
-                    "location": row.cells[LOCATION_INDEX].text
+                    "name": row.cells[NAME_INDEX].text.strip(),
+                    "days": row.cells[DAYS_INDEX].text.strip(),
+                    "dates": row.cells[DATES_INDEX].text.strip(),
+                    "time": row.cells[TIME_INDEX].text.strip(),
+                    "location": row.cells[LOCATION_INDEX].text.strip()
                 })
     with open("clubs.json", 'w') as outfile:
         json.dump(clubs, outfile)
