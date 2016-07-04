0131e84c587a3073134c636e4f1306cd2052dc92
Karina Antonio <Karina Antonio>
2016-07-03 20:32:45 -0700

    Scrape successful for the current month. JSON files are created.

--- da_events_calendar_scraper.py
+++ da_events_calendar_scraper.py
@@ -1,50 +1,64 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
-from lxml import etree
 import json
 
-def find_calendar_events(url):
-    print "Requesting {}...".format(url)
-    r = requests.get(url)
-    with open("calendar.html", 'w') as outfile:
-        outfile.write(r.text):w
-        K
-    print "Calendar downloaded. Parsing..."
-    calendar_soup = BeautifulSoup(r.text, "html.parser")
+BASE_URL = "https://www.deanza.edu/eventscalendar/"
 
+def find_calendar_events(calendar_html):
+    calendar_soup = BeautifulSoup(calendar_html, "html.parser")
     events = []
     calendar_body = calendar_soup.find("table", class_="main")
-    # skip the first tr, which is the header
+    # Skip the first tr, which is the header
     for tr in calendar_body.find_all("tr")[1:]:
         for td in tr.find_all("td"):
             for a in td.find_all("a"):
-                if a["class"] == "entry":
-                    events.append(parse_event_page(BASE_CALENDAR_URL + a["href"]))
+                # "class" is a special multi-valued attribute, so it's contained in a list
+                if a["class"][0] == "entry":
+                    r = requests.get(BASE_URL + a["href"])
+                    events.append(parse_event_page(r.text))
 
     return events
 
-def parse_event_page(url):
-    r = requests.get(url)
-    event_soup = BeautifulSoup(r.text, "html.parser")
-    EVENT_FIELDS = ["Description", "Date", "Time", "Location", "Sponsor"]
-    event = {}
+def parse_event_page(event_html):
+    event_soup = BeautifulSoup(event_html, "html.parser")
+    EVENT_FIELDS = ["Description:", "Date:", "Time:", "Location:", "Sponsor:"]
+    event = {
+        "name": event_soup.find(id="cal_div_obj").h2.get_text(),
+        "source": "DA Calendar"
+    }
 
-    event["name"] = event_soup.find(id="cal_div_obj").h2.get_text()
-
-    for tr in event_soup.find("tbody"):
-        field_name = tr.contents[0].get_text()
+    for tr in event_soup.find("table").find_all("tr"):
+        field_name = tr.contents[0].get_text().strip()
         if field_name in EVENT_FIELDS:
-            field_value = tr.contents[1].get_text()
-            event[field_name.lower()] = field_value
+            field_value = tr.contents[1].get_text().strip()
+            event[field_name[:-1].lower()] = field_value
 
     return event
 
-if __name__ == "__main__"::w:w
-K:w
+def main():
+    def download_calendar_html():
+        THIS_MONTH = BASE_URL + "month.php"
+        print "Requesting {}...".format(THIS_MONTH)
+        r = requests.get(THIS_MONTH)
+        with open("calendar.html", 'w') as outfile:
+            outfile.write(r.text)
+        print "Calendar downloaded."
 
+        return r.text
 
-    BASE_URL = "https://www.deanza.edu/eventscalendar/"
+    def save_events_json(events):
+        with open("calendar_events.json", 'w') as outfile:
+            json.dump(events, outfile)
 
-    with open("calendar_events.json", 'w') as outfile:
-        json.dump(find_calendar_events(BASE_URL + "month.php"), outfile)
+    if len(sys.argv) == 2:
+        save_events_json(find_calendar_events(sys.argv[1]))
+    elif len(sys.argv) == 1:
+        save_events_json(find_calendar_events(download_calendar_html()))
+    else:
+        print "Invalid argument list. Exiting..."
+        sys.exit(2)
     print "Parsing complete! Saved to calendar_events.json."
+
+if __name__ == "__main__":
+    main()
