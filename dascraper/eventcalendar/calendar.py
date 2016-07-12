0db827ed690141bf8105497737bbc5b417e3eced
Karina Antonio <karinafantonio@gmail.com>
2016-07-12 14:43:48 -0700

    Fixed typo in previous import modification

--- dascraper/eventcalendar/calendar.py
+++ dascraper/eventcalendar/calendar.py
@@ -3,7 +3,7 @@ import json
 import logging
 import requests
 from bs4 import BeautifulSoup
-from dascraper.eventscalendar import event
+from dascraper.eventcalendar import event
 
 
 BASE_URL = "https://www.deanza.edu/eventscalendar/"
